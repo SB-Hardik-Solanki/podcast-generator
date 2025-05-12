348c84c07acfb4f33d6a9437b3e30e6c7485ec26
Hardik Solanki <hardik.solanki@smartbear.com>
2025-05-12 10:26:36 +0000

    Modified files

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workplace
 
-python3 /user/bin/feed.py
+python3 /usr/bin/feed.py
 
 git add -A && git commit -m "Update feed"
 
